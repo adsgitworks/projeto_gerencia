494166f2ec1fa12dece37112a1be78fd06646725
jpro <joaopfvasconcelosxd@gmail.com>
2019-02-28 19:16:18 -0300

    implementando funcoes de busca

---
+++ frontend/chat_page/assets/js/utils.js
@@ -0,0 +1,12 @@
+function id(id){
+    return document.getElementById(id)
+}
+function createElement(element){
+    return document.createElement(element)
+}
+function setID(element,id){
+    element.id = id
+}
+function setClass(element,classname){
+    element.className = classname
+}
