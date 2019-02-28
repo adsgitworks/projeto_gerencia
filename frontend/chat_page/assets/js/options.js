28429d3d49da335fb952e79394a80ea76ea4c13d
klihsman <klihsman2.0@gmail.com>
2019-02-28 19:25:21 -0300

    implemetando funcoes no evento clique

--- frontend/chat_page/assets/js/options.js
+++ frontend/chat_page/assets/js/options.js
@@ -15,4 +15,30 @@
         c = 0
     }
 
+    bt_user.onclick = function(){
+
+        function animating(){
+            let fontsize = 0;
+            let speed = 8.25
+            function Tamanho(alturaMax, LarguraMax,tmp){
+                
+                var obj=id('float_parent')
+        
+                if((obj.offsetHeight<=alturaMax)&&(obj.offsetWidth<=LarguraMax)){
+                //setting left position
+                obj.style.left = `${obj.offsetLeft - speed}px`
+                //
+                obj.style.width=((obj.offsetWidth+=10)+"px");
+                obj.style.height=((obj.offsetHeight+=10)+"px");
+                    if(obj.style.fontSize != '17px'){
+                        fontsize += 1
+                        obj.style.fontSize = `${fontsize}px`
+                    }
+                }
+                else  clearInterval(tmp);
+            }
+            tmp= setInterval(() =>Tamanho(200,200,tmp), 5);
+        }
+    }
+
 })()
