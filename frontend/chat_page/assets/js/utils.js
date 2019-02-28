e0a2435f638bf94c6a8dff95b66734e9873c9fab
jpro <joaopfvasconcelosxd@gmail.com>
2019-02-28 19:19:17 -0300

    funcoes que utilizam arrays e xhr

--- frontend/chat_page/assets/js/utils.js
+++ frontend/chat_page/assets/js/utils.js
@@ -28,3 +28,27 @@ function getFromName(name,i){
 function getFromSelector(selector){
     return document.querySelector(selector)
 }
+function getArray(obj){
+    return Array.from(obj)
+}
+function doSomething(array,callback){
+    array.forEach( e => {
+        callback(e)
+    })
+}
+function JsonPostRequest(url,data){
+    return new Promise((resolve,reject) => {
+        const xhr = new XMLHttpRequest()
+        xhr.open('POST',url,true)
+
+        xhr.onreadystatechange = function(){
+            if(xhr.status == 200 && xhr.readyState == 4){
+                resolve(xhr.responseText)
+            }
+        }
+
+        xhr.setRequestHeader('Content-Type','application/json')
+        xhr.send(data)
+
+    })
+}
